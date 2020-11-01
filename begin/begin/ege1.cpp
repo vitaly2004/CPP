9b59525082e3e4a3f95cfb6cfd10d4e6a46c162a
Qvocha <43353220+Qvocha@users.noreply.github.com>
2020-11-01 19:31:12 +0300

    bbhbh

--- begin/begin/ege1.cpp
+++ begin/begin/ege1.cpp
@@ -1,61 +1,86 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int* array, int const size)
-{
-	for (int i = 0; i < size; ++i)
-	{
-		for (int j = 0; j < size - 1; ++j)
-		{
-			if (array[j] > array[j + 1])
-			{
-				swap(array[j], array[j + 1]);
-			}
-		}
-	}
-}
- 
-void printArray(int const* array, int const size)
-{
-	for (int i = 0; i < size; ++i)
-	{
-		cout << array[i] << " ";
-	}
-	cout << endl;
-}
-
-void inputArray(int* array, int const size)
-{
-	for (int i = 0; i < size; ++i)
-	{
-		cin >> array[i];
-	}
-}
-
-void mmmmain()
-{
-	int const n = 5;
-	int a[n];
-	int sum = 0;
-	int sum1 = 0;
-	int k = 0;
-	cin >> sum;
-	for (int i = 0; i < n; ++i)
-	{
-		if (sum1 <= sum)
-		{
-			sum1 = sum1 + a[i];
-			++k;
-		}
-	}
-	cout << a[n - 2] << " " << k - 1;
-}
-
 int main()
 {
-	int a[5];
-	
-	inputArray(a, 5);
+	int n = 0;
+	cin >> n;
+	int max171 = 0;
+	int max172 = 0;
+	int max0 = 0;
+	int mmax171 = 0;
+	int mmax172 = 0;
+	int mmax0 = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		int num = 0;
+		cin >> num;
+		if (num % 17 == 0 and num % 2 == 0)
+		{
+			if (num >= max171)
+			{
+				max172 = max171;
+				max171 = num;
+			}
+			if (num < max171 and num > max172)
+			{
+				max172 = num;
+			}
+		}
+		else if ((num - max171) % 2 == 0)
+		{
+			max0 = num;
+		}
 
-	printArray(a, 5);
+		if (num % 17 == 0 and num % 2 != 0)
+		{
+			if (num >= mmax171)
+			{
+				mmax172 = mmax171;
+				mmax171 = num;
+			}
+			if (num < mmax171 and num > mmax172)
+			{
+				mmax172 = num;
+			}
+		}
+		else if ((num - mmax171) % 2 == 0)
+		{
+			mmax0 = num;
+		}
+	}
+	int max[4] = {0, 0, 0, 0};
+	if (max171 != 0 and max172 != 0)
+	{
+		max[0] = max171 + max172;
+	}
+	if (max171 != 0 and max0 != 0)
+	{
+		max[1] = max171 + max0;
+	}
+	if (mmax171 != 0 and mmax172 != 0)
+	{
+		max[2] = mmax171 + mmax172;
+	}
+	if (mmax171 != 0 and mmax0 != 0)
+	{
+		max[3] = mmax171 + mmax0;
+	}
+	int mmmax = 0;
+	int ind = 0;
+	for (int i = 0; i < 4; ++i)
+	{
+		if (max[i] > mmmax)
+		{
+			mmmax = max[i];
+			ind = i;
+		}
+	}
+	if (mmmax == 0)
+	{
+		cout << "0 0";
+		return 0;
+	}
+	
+	return 0;
 }
